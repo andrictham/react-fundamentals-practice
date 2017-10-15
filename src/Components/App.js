29c32fae31e98d5691a35eb2262f8657741eeac7
Andric Tham <andric@nuisance.co>
2017-10-15 15:50:06 +0800

    Create Popular component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Popular from './Popular'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello world!
+        <Popular/>
       </div>
     );
   }
 }
 
-export default App;
+export default App
