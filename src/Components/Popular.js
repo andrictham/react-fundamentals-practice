bbee370d42965e8a11153b6badb860b17352cb86
Andric Tham <andric@nuisance.co>
2017-10-18 20:07:34 +0800

    <Popular />: Active tab is now highlighted

--- src/Components/Popular.js
+++ src/Components/Popular.js
@@ -2,6 +2,20 @@ import React from 'react'
 import './Popular.css'
 
 class Popular extends React.Component {
+  constructor(props){
+  	super(props)
+  	this.state = {
+      selectedLanguage: 'All'
+    }
+    this.updateLanguage = this.updateLanguage.bind(this)
+  }
+
+  updateLanguage(language){
+    this.setState({
+      selectedLanguage: language
+    })
+  }
+
   render() {
     const languages = [
       'All',
@@ -13,10 +27,16 @@ class Popular extends React.Component {
     ]
     return (
       <ul className='languages'>
-        {languages.map((lang) =>{
+        {languages.map((language) =>{
           return (
-            <li key={lang}>
-              {lang}
+            <li
+              key={language}
+              onClick={this.updateLanguage.bind(null, language)}
+              // Our function is already bound in constructor, so `null` tells it to respect that earlier binding, but go ahead and create a new function with `language` passed in as an argument. https://stackoverflow.com/questions/29810914/react-js-onclick-cant-pass-value-to-method
+              className={
+                this.state.selectedLanguage === language ? "active" : ""
+              }>
+              {language}
             </li>
           )
         })}
