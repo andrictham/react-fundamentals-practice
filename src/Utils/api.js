5a34b53a53d94465e929a4b32cf59939a8bcd792
Andric Tham <andric@nuisance.co>
2017-10-19 00:30:33 +0800

    Utils: Fix typo in axios call

--- src/Utils/api.js
+++ src/Utils/api.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 
 const fetchPopularRepos = (language) => {
   const encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`)
-  return axios.get(encodeURI).then( (response) => {
+  return axios.get(encodedURI).then( (response) => {
     return response.data.items
   })
 }
