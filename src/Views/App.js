fd2b6ef0331c979cc45df1f77b4abd5317e3f6d4
Andric Tham <andric@nuisance.co>
2017-10-22 20:16:52 +0800

    <App />: Define routes for homepage, /battle, and /popular

--- src/Views/App.js
+++ src/Views/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
 import './App.css'
-import Popular from '../Components/Popular'
+import Nav from '../Components/Nav'
+import Home from '../Views/Home'
+import Battle from '../Views/Battle'
+import Popular from '../Views/Popular'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Popular/>
-      </div>
+      <Router>
+        <div className="App">
+          <Nav />
+          <Route exact path="/" component={Home}></Route>
+          <Route path="/battle" component={Battle}></Route>
+          <Route path="/popular" component={Popular}></Route>
+        </div>
+      </Router>
     );
   }
 }
