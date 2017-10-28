386c5c13f37ce3fabeae99a4f22d7b04a93af9cc
Andric Tham <andric@nuisance.co>
2017-10-29 01:05:19 +0800

    <Results />: Render Github API response to the page, along with any errors and loading states

--- src/Views/Results.js
+++ src/Views/Results.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import queryString from 'query-string'
 import { battle } from '../Utils/api'
 
@@ -16,13 +17,43 @@ class Results extends Component {
 		const { location } = this.props
 		const players = queryString.parse(location.search)
 		battle([players.playerOne, players.playerTwo]).then(results => {
-			console.log('Results: ', results)
+			// If there was an error, update our state with an error message.
+			if (results === null) {
+				return this.setState(() => {
+					return {
+						error:
+							'Looks like there was an error! Check that both users exist on Github?',
+						loading: false,
+					}
+				})
+			}
+
+			// If not, update our state with the winner and loser.
+			this.setState(() => {
+				return {
+					error: null,
+					winner: results[0],
+					loser: results[1],
+					loading: false,
+				}
+			})
 		})
 	}
 	render() {
-		const { error, winner, loser, loading } = this.state
+		let { error, winner, loser, loading } = this.state
 		if (loading) {
 			return <p>Loading</p>
+		}
+		if (error) {
+			return (
+				<div>
+					<p> {error} </p>
+					<Link to="/battle">Reset</Link>
+				</div>
+			)
+		}
+		if (!loading && !error) {
+			return <div>{JSON.stringify(this.state, null, 2)}</div>
 		}
 	}
 }
