27df00f168a1856fdbb8ea8664189c0f1cf85b29
Andric Tham <andric@nuisance.co>
2017-10-28 17:06:34 +0800

    <Battle />: Make <PlayerInput /> component

--- src/Views/Battle.js
+++ src/Views/Battle.js
@@ -3,6 +3,59 @@ import PropTypes from 'prop-types'
 
 import './Battle.css'
 
+class PlayerInput extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			username: '',
+		}
+		this.handleChange = this.handleChange.bind(this)
+		this.handleSubmit = this.handleSubmit.bind(this)
+	}
+	handleChange(event) {
+		const { value } = event.target
+		this.setState(() => {
+			return {
+				username: value, // store form value
+			}
+		})
+	}
+	handleSubmit(event) {
+		event.preventDefault()
+		this.props.onSubmit(this.props.id, this.state.username)
+	}
+	render() {
+		return (
+			<form className="column" onSubmit={this.handleSubmit}>
+				<label htmlFor="username" className="header">
+					{this.props.label}
+				</label>
+				<input
+					type="text"
+					id="username"
+					placeholder="github username"
+					autoComplete="off"
+					value={this.state.username} // get form value
+					onChange={this.handleChange} // set form value
+				/>
+				<button
+					className="button"
+					type="submit"
+					disabled={!this.state.username} // disable if form value if empty
+				>
+					Submit
+				</button>
+			</form>
+		)
+	}
+}
+
+PlayerInput.propTypes = {
+	id: PropTypes.string.isRequired,
+	label: PropTypes.string.isRequired,
+	onSubmit: PropTypes.func.isRequired,
+}
+
 class Battle extends Component {
 	constructor(props) {
 		super(props)
