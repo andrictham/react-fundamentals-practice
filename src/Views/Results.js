8211012cddbe479079efbd83bee2b34f37a73d3b
Andric Tham <andric@nuisance.co>
2017-10-29 00:53:28 +0800

    <Results />: Scaffold component

--- src/Views/Results.js
+++ src/Views/Results.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react'
+import queryString from 'query-string'
+import { battle } from '../Utils/api'
 
 class Results extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			winner: null,
+			loser: null,
+			error: null,
+			loading: true,
+		}
+	}
+	componentDidMount() {
+		const { location } = this.props
+		const players = queryString.parse(location.search)
+		battle([players.playerOne, players.playerTwo]).then(results => {
+			console.log('Results: ', results)
+		})
+	}
 	render() {
-		console.log(this.props)
-		return <div>Results</div>
+		const { error, winner, loser, loading } = this.state
+		if (loading) {
+			return <p>Loading</p>
+		}
 	}
 }
 
