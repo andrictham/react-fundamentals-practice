78f662bb49b9673721e1dd9d2937f84b08d581af
Andric Tham <andric@nuisance.co>
2017-10-28 23:37:55 +0800

    <Results />: Set up route for /battle/results

--- src/Views/App.js
+++ src/Views/App.js
@@ -1,35 +1,35 @@
 import React, { Component } from 'react'
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css'
 import Nav from '../Components/Nav'
 import Home from '../Views/Home'
 import Battle from '../Views/Battle'
+import Results from '../Views/Results'
 import Popular from '../Views/Popular'
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <Nav />
-          {/* What Switch does is, instead of rendering all routes that are active, Switch is going to render just one specific route – the very first one it finds a match for. This allous us to specify a 404 page. */}
-          <Switch>
-            <Route exact path="/" component={Home}></Route>
-            <Route exact path="/battle" component={Battle}></Route>
-            <Route path="/popular" component={Popular}></Route>
-            {/* We can pass in a render prop to <Route />, which takes in a functional component, to tell <Router /> to render something when no match is found */}
-            <Route render={ () => {
-              return <p>Not found</p>
-            }} />
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<div className="App">
+					<Nav />
+					{/* What Switch does is, instead of rendering all routes that are active, Switch is going to render just one specific route – the very first one it finds a match for. This allous us to specify a 404 page. */}
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route exact path="/battle" component={Battle} />
+						<Route path="/battle/results" component={Results} />
+						<Route path="/popular" component={Popular} />
+						{/* We can pass in a render prop to <Route />, which takes in a functional component, to tell <Router /> to render something when no match is found */}
+						<Route
+							render={() => {
+								return <p>Not found</p>
+							}}
+						/>
+					</Switch>
+				</div>
+			</Router>
+		)
+	}
 }
 
 export default App
