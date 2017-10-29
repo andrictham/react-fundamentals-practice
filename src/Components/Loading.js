7b2f469eb833fdec216e31e3679e0cbce844c4fd
Andric Tham <andric@nuisance.co>
2017-10-29 11:32:37 +0800

    <Loading />: Implement logic to animate ...

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 const styles = {
-	content: {
+	loadingText: {
 		textAlign: 'center',
 		fontSize: '35px',
 	},
@@ -15,17 +15,45 @@ class Loading extends Component {
 			text: props.text,
 		}
 	}
+
+	componentDidMount() {
+		let endState = this.props.text + '...'
+		this.interval = window.setInterval(() => {
+			// If end state has been reached (an elipsis), reset text
+			if (this.state.text === endState) {
+				this.setState(() => {
+					return {
+						text: this.props.text,
+					}
+				})
+				// else, keep appending periods to it
+			} else {
+				// Take our previous state and add to it
+				this.setState(prevState => {
+					return {
+						text: prevState.text + '.',
+					}
+				})
+			}
+		}, this.props.interval)
+		// Run every 300ms, or whatever timing is specified
+	}
+
 	render() {
-		return <p style={styles.content}>{this.state.text}</p>
+		const { loadingText } = styles
+		let { text } = this.state
+		return <p style={loadingText}>{text}</p>
 	}
 }
 
 Loading.propTypes = {
 	text: PropTypes.string.isRequired,
+	interval: PropTypes.number.isRequired,
 }
 
 Loading.defaultProps = {
 	text: 'Loading',
+	interval: 300,
 }
 
 export default Loading
